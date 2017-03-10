6c9d6f709ab02eb3098e4c4bfaa1d4bdc5f53bd2
Witoon <alive-@msn.com>
2017-03-10 09:54:18 +0700

    Fix Db creation didn't working

--- TechTileNimation/Models/AppDbContext.cs
+++ TechTileNimation/Models/AppDbContext.cs
@@ -11,6 +11,8 @@ namespace TechTileNimation.Models
     {
         public DbSet<SensationEntry> SensationEntry { get; set; }
 
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<SensationEntry>();
