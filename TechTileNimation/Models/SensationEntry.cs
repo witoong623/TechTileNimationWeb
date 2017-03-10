81ca2695930da42ddda0a766ce65928617941c15
Witoon <alive-@msn.com>
2017-03-10 22:19:03 +0700

    Add SafeName property to model, add auto play and pause function

--- TechTileNimation/Models/SensationEntry.cs
+++ TechTileNimation/Models/SensationEntry.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,5 +13,21 @@ namespace TechTileNimation.Models
         public string PreviewImageLink { get; set; }
         public string SensationSoundLink { get; set; }
         public string AnimationLink { get; set; }
+
+        private string _safeName;
+
+        [NotMapped]
+        public string SafeName
+        {
+            get
+            {
+                if (_safeName == null)
+                {
+                    _safeName = Name.Replace(" ", "_");
+                }
+
+                return _safeName;
+            }
+        }
     }
 }
