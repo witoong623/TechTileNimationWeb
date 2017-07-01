a4f8839d820e6edde9b014b3177a5bc8497abc27
Witoon <alive-@msn.com>
2017-07-01 11:31:24 +0700

    Ported to .NET Core 1.1 and deleted  Application Insight package.

--- TechTileNimation/Program.cs
+++ TechTileNimation/Program.cs
@@ -16,7 +16,6 @@ namespace TechTileNimation
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
-                .UseApplicationInsights()
                 .Build();
 
             host.Run();
