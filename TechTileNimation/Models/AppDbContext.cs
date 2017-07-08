51fd88e74eb8898d0cdf4fcbccd4c141a5bce3e1
Witoon <alive-@msn.com>
2017-07-08 13:44:29 +0700

    Changed database from SQL Server to MariaDB.

--- TechTileNimation/Models/AppDbContext.cs
+++ TechTileNimation/Models/AppDbContext.cs
@@ -1,9 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using TechTileNimation.Models;
 
 namespace TechTileNimation.Models
 {
@@ -15,7 +10,35 @@ namespace TechTileNimation.Models
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<SensationEntry>();
+            modelBuilder.Entity<SensationEntry>()
+                .ToTable("sense_entry")
+                .HasKey(s => s.Id)
+                .HasName("se_id");
+
+            modelBuilder.Entity<SensationEntry>()
+                .Property(s => s.Id)
+                .HasColumnName("se_id")
+                .HasColumnType("INT");
+
+            modelBuilder.Entity<SensationEntry>()
+                .Property(s => s.Name)
+                .HasColumnName("se_name")
+                .HasColumnType("VARCHAR(50)");
+
+            modelBuilder.Entity<SensationEntry>()
+                .Property(s => s.PreviewImageLink)
+                .HasColumnName("se_image_link")
+                .HasColumnType("VARCHAR(150)");
+
+            modelBuilder.Entity<SensationEntry>()
+                .Property(s => s.SensationSoundLink)
+                .HasColumnName("se_sound_link")
+                .HasColumnType("VARCHAR(150)");
+
+            modelBuilder.Entity<SensationEntry>()
+                .Property(s => s.AnimationLink)
+                .HasColumnName("se_animation_link")
+                .HasColumnType("VARCHAR(150)");
         }
     }
 }
--- TechTileNimation/Models/SensationEntry.cs
+++ TechTileNimation/Models/SensationEntry.cs
@@ -6,12 +6,22 @@ using System.Threading.Tasks;
 
 namespace TechTileNimation.Models
 {
+    [Table("sense_entry")]
     public class SensationEntry
     {
+        [Column("se_id", TypeName = "INT")]
         public int Id { get; set; }
+
+        [Column("se_name", TypeName = "VARCHAR(50)")]
         public string Name { get; set; }
+
+        [Column("se_image_link", TypeName = "VARCHAR(150)")]
         public string PreviewImageLink { get; set; }
+
+        [Column("se_sound_link", TypeName = "VARCHAR(150)")]
         public string SensationSoundLink { get; set; }
+
+        [Column("se_animation_link", TypeName = "VARCHAR(150)")]
         public string AnimationLink { get; set; }
 
         private string _safeName;
